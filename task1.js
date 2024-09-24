b22675248ec98270081846c23d83ecf41ce4c94c
TanyaMykhaylyuk <tanyamykhaylyuk27@gmail.com>
2024-09-24 11:52:36 +0300

    task 11

--- task1.js
+++ task1.js
@@ -5,3 +5,21 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+
+    const usersList = document.querySelector('ul.usersList');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        users.forEach(user => {
+            const li = document.createElement("li");
+            li.textContent = user.name;
+            usersList.appendChild(li);
+        });
+    })
+
+    
+
+
